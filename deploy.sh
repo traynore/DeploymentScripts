15f1545ce9d2b8c7903d083c6947bd081d4b6ff8
traynore <emmett.traynor@gmail.com>
2015-01-06 23:22:40 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ timestamp() {
 	date +"%T"
 }
 
-# log timestamp to file to make errors more identifiable
+# apply timestamp to logs
 echo $(timestamp) >> ~/MyLogs/deploy_log.txt
 
 # create sandbox
@@ -36,15 +36,15 @@ apt-get -q -y remove mysql-client mysql-server
 echo mysql-server mysql-server/root_password password password | debconf-set-selections
 echo mysql-server mysql-server/root_password_again password password Z debconf-set-selections
 
-# update apt repo
+# update
 apt-get update
 
-# reinstall
+# reinstall apache and mySQL
 echo "Reinstalling Apache and mySQL" >> ~/MyLogs/deploy_log.txt
 apt-get -q -y install apache2
 apt-get -q -y install mysql-client mysql-server
 
-# restart services
+# restart the services
 echo "Restarting services" >> ~/MyLogs/deploy_log.txt
 service apache2 start
 service mysql start
@@ -54,12 +54,12 @@ cd ~
 # untar app
 tar -zxvf webpackage_preDeploy.tgz WebApp
 
-# move components to /www and /cgi-bin
-echo "Moving components" >> ~/MyLogs/deploy_log.txt
+# move files to /www and /cgi-bin
+echo "Moving files" >> ~/MyLogs/deploy_log.txt
 cp ~/WebApp/www/* /var/www
 cp ~/WebApp/cgi-bin/* /usr/lib/cgi-bin
 
-# make scripts executable
+# change ownership to make scripts executable
 chmod a+x /usr/lib/cgi-bin/*
 
 # test necessary files are in place
@@ -75,13 +75,12 @@ else
 	exit
 fi
 
-# configure crontab to run monitoring script
-
+# crontab to run a monitoring script
 
 echo "Setting up monitoring Cron job" >> ~/MyLogs/deploy_log.txt
 (crontab -l ; echo "40 15 * * * bash ~/logmon.sh") | uniq - | crontab -
 
-# run monitoring script
+# run monitoring script as above doesn't seem to work
 bash ~/logmon.sh
 
 echo "A new version of your Web Application has been successfully deployed LIVE" >> ~/MyLogs/deploy_log.txt | perl ~/sendmail.pl $ADMINISTRATOR $MAILSERVER
